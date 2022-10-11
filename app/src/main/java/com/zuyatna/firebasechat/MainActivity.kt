deb07fab93ea6998bc325e9402d7e81dc32484b3
suyatna <zuyatna@gmail.com>
2022-10-11 16:03:25 +0700

    adding peripheral

--- app/src/main/java/com/zuyatna/firebasechat/MainActivity.kt
+++ app/src/main/java/com/zuyatna/firebasechat/MainActivity.kt
@@ -2,10 +2,37 @@ package com.zuyatna.firebasechat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.zuyatna.firebasechat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.sign_out_menu -> {
+                signOut()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun signOut() {
+
     }
 }
